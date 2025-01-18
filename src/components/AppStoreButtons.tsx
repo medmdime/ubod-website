c01f4a60105a1a4b1738667ce23f544a985d5cde
mohamedmdimagh <mohamdimagh1@gmail.com>
2025-01-18 19:12:08 +0100

    feat fixing the website

--- src/components/AppStoreButtons.tsx
+++ src/components/AppStoreButtons.tsx
@@ -15,9 +15,9 @@ const AppStoreButtons: React.FC = () => {
           <div className="text-xl font-semibold">{t("hero.appStoreButton.line2")}</div>
         </div>
       </button>
-      <a href='https://play.google.com/store/apps/details?id=com.ubod'>
+      <a  href='https://play.google.com/store/apps/details?id=com.ubod'>
 
-      <button className="flex items-center justify-center bg-darkGreen text-white px-6 py-3 rounded-lg hover:bg-gray-800 transition-colors duration-300">
+      <button className="flex w-full items-center justify-center bg-darkGreen text-white px-6 py-3 rounded-lg hover:bg-gray-800 transition-colors duration-300">
         <svg className="w-8 h-8 mr-2" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
           <path fill="currentColor" d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"/>
         </svg>
